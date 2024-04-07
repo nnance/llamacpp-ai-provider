ff06fa56353bc7387c648adfeae4ac609683a5a3
Nick Nance <nance.nick@gmail.com>
2024-04-07 13:05:38 +0700

    remove basic bindings example

--- src/examples/bindings.ts
+++
@@ -1,4 +0,0 @@
-import { LLamacppBindings } from "../bindings.js";
-
-const bindings = new LLamacppBindings("Nick");
-bindings.greet("Nick");
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,1 @@
 export { LLamaCpp } from "./llamacpp/llamacpp-facade.js";
-export { LLamacppBindings } from "./bindings.js";
