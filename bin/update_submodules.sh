793e456d039d277cb383d2480bd687672a9ba4f4
Nick Nance <nance.nick@gmail.com>
2024-04-07 20:46:54 +0700

    pin llama.cpp to a compatible tag

--- bin/update_submodules.sh
+++ bin/update_submodules.sh
@@ -4,7 +4,10 @@
 REPO_URL="https://github.com/ggerganov/llama.cpp.git"
 
 # Define the directory where the repository will be cloned
-DIR="llamacpp/lib"
+DIR="lib/llamacpp"
+
+# Define the compatible tag
+TAG="b1961"
 
 # If the directory already exists, pull the latest changes
 if [ -d "$DIR" ]; then
@@ -15,5 +18,6 @@ else
   # If the directory does not exist, clone the repository
   echo "Directory $DIR does not exist."
   echo "Cloning repository..."
-  git clone "$REPO_URL" "$DIR"
+  # To git clone a repository at a certain tag, you can use the following command:
+  git clone --branch "$TAG" --single-branch "$REPO_URL" "$DIR"
 fi
